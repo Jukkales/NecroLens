b942cf68abdf8648fa31c3b41e6eef40f2521cd1
Jukkales <Jukkales@users.noreply.github.com>
2024-05-03 22:50:44 +0200

    reverting stupidity

--- NecroLens/NecroLens.cs
+++ NecroLens/NecroLens.cs
@@ -1,12 +1,11 @@
 ﻿#undef DEBUG
 
-using System.Collections;
+
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
 using ECommons;
-using ECommons.Reflection;
 using NecroLens.Model;
 using NecroLens.Service;
 using NecroLens.Windows;
@@ -18,9 +17,6 @@ namespace NecroLens;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public sealed class NecroLens : IDalamudPlugin
 {
-    private const string OldRepoUrl = "https://raw.githubusercontent.com/Jukkales/DalamudPlugins/master/repo.json";
-    private const string NewRepoUrl = "https://puni.sh/api/repository/jukka";
-    
     private readonly ConfigWindow configWindow;
     private readonly DeepDungeonService deepDungeonService;
     private readonly ESPService espService;
@@ -70,8 +66,6 @@ public sealed class NecroLens : IDalamudPlugin
             Dalamud.ClientLanguage.Japanese => CultureInfo.GetCultureInfo("ja"),
             _ => CultureInfo.GetCultureInfo("en")
         };
-        
-        TryUpdateRepo();
     }
 
     public void Dispose()
@@ -110,27 +104,5 @@ public sealed class NecroLens : IDalamudPlugin
     public void ShowConfigWindow()
     {
         configWindow.IsOpen = true;
-    }
-    
-    private void TryUpdateRepo()
-    {
-        var conf = DalamudReflector.GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
-        var repos = (IEnumerable)conf.GetFoP("ThirdRepoList");
-        if (repos != null)
-        {
-            foreach (var r in repos)
-                if (OldRepoUrl.EqualsIgnoreCase((string)r.GetFoP("Url")))
-                {
-                    PluginLog.Information("Updating repository URL");
-                    var pluginMgr = DalamudReflector.GetPluginManager();
-                    Safe(() =>
-                    {
-                        r.SetFoP("Url", NewRepoUrl);
-                        conf.Call("Save", []);
-                        pluginMgr.Call("SetPluginReposFromConfigAsync", [true]);
-                    });
-                    return;
-                }
-        }
     }
 }
